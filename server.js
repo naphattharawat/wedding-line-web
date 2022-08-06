d68fc7540499e95602a3734ed23e3d7ae6781ca2
naphattharawat <naphattharawat@gmail.com>
2022-08-06 11:08:29 +0700

    update

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express')
 var path = require('path');
 var app = express()
 
-app.use(express.static(path.join(__dirname, './dist/web/')));
+app.use(express.static(path.join(__dirname, './dist/')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './dist/web/index.html'));
+  res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
 // catch 404 and forward to error handler
